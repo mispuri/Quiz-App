73e273fa9ac7c6d541bace77ddaf77bf0be419c2
mispuri <charrychurry@gmail.com>
2024-01-12 20:51:14 -0500

    creacion de preguntas y logica del quiz app

---
+++ app/js/questions.js
@@ -0,0 +1,107 @@
+export const questions = [
+  {
+    question: 'Which is larget animal in the world ?',
+    answers: [
+      {
+        options: 'Shark',
+        correct: false,
+      },
+      {
+        options: 'Blue whale',
+        correct: true,
+      },
+      {
+        options: 'Elephant',
+        correct: false,
+      },
+      {
+        options: 'Giraffe',
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: 'Which is the biggest country in the world ?',
+    answers: [
+      {
+        options: 'Colombia',
+        correct: false,
+      },
+      {
+        options: 'Unite States',
+        correct: false,
+      },
+      {
+        options: 'Swiss',
+        correct: false,
+      },
+      {
+        options: 'Russia',
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: 'Which is the smallest country in the world ?',
+    answers: [
+      {
+        options: 'Vatican City',
+        correct: true,
+      },
+      {
+        options: 'Buthan',
+        correct: false,
+      },
+      {
+        options: 'Nepal',
+        correct: false,
+      },
+      {
+        options: 'Shri Lanka',
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: 'Which is largest desert in the world ?',
+    answers: [
+      {
+        options: 'Kalahari',
+        correct: false,
+      },
+      {
+        options: 'Gobi',
+        correct: false,
+      },
+      {
+        options: 'Sahara',
+        correct: false,
+      },
+      {
+        options: 'Antarctica',
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: 'Which is the smallest continent in the world ?',
+    answers: [
+      {
+        options: 'Asia',
+        correct: false,
+      },
+      {
+        options: 'Australia',
+        correct: true,
+      },
+      {
+        options: 'Arctic',
+        correct: false,
+      },
+      {
+        options: 'Africa',
+        correct: false,
+      },
+    ],
+  },
+];
